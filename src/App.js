3c60578acac5d0680e6b9fc2e28677c90906f5fa
Mikita Dubovik <Mikita_Dubovik@epam.com>
2019-12-19 16:43:11 +0300

    Beggining of body

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './App.scss';
 
 import Header from './components/header/header.jsx'
+import Body from './components/body/body.jsx'
 
-function App() {
-  return (
-    <div className="App">
-      <Header param ="123" />
-    </div>
-  );
+class App extends React.Component{
+  render(){
+    return (
+      <div className="App">
+        <Header />
+        <Body />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,23 +3,28 @@ import './header.scss';
 
 import Button from '../button/button.jsx'
 
-const Header = () => (
-    <div className="head">
-        <div className="mainHade">
-            <h2>
-                FIND YOUR MOVIE
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="head">
+                <div className="mainHade">
+                    <h2>
+                        FIND YOUR MOVIE
         </h2>
-            <input type="text" placeholder="Quentin Tarantino"></input>
-            <Button />
-            <h4>
-                SEARCH BY
-        </h4>
-            <div className="toggle">
-                <a className="active" href="#title">TITLE</a>
-                <a href="#genre">GENRE</a>
+                    <input type="text" placeholder="Quentin Tarantino"></input>
+                    <Button />
+                    <br />
+                    <h4>
+                        SEARCH BY
+                </h4>
+                    <div className="toggle">
+                        <a className="active" href="#title">TITLE</a>
+                        <a href="#genre">GENRE</a>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-);
+        );
+    }
+}
 
 export default Header;
