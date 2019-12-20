eae71b00be861c2f4a51d5b212fdad1715162808
Mikita Dubovik <Mikita_Dubovik@epam.com>
2019-12-20 14:48:38 +0300

    Recreating app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import './index.module.css';
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store} >
+    <BrowserRouter>
+    <Router />
+  </BrowserRouter>
+  </Provider>
+  ,
+  document.getElementById('root')
+);
